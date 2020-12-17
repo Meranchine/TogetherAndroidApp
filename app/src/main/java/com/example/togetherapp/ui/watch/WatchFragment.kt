1e5ec7cd935a13483e55613369b2a74ad469a83d
Meranchine <meranchine@163.com>
2020-12-17 14:16:19 +0800

    Watch

--- app/src/main/java/com/example/togetherapp/ui/watch/WatchFragment.kt
+++ app/src/main/java/com/example/togetherapp/ui/watch/WatchFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.example.togetherapp.R
+import kotlinx.android.synthetic.main.watch_fragment.*
 
 class WatchFragment : Fragment() {
 
@@ -27,6 +29,21 @@ class WatchFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(WatchViewModel::class.java)
         // TODO: Use the ViewModel
+        viewModel.seconds.observe(viewLifecycleOwner,Observer{
+            val hours = it/3600
+            val minutes = (it % 3600)/60
+            val secs = it % 60
+            timer.text = String.format("%02d:%02d:%02d",hours,minutes,secs)
+        })
+        start.setOnClickListener {
+            viewModel.start()
+        }
+        stop.setOnClickListener {
+            viewModel.stop()
+        }
+        restart.setOnClickListener {
+            viewModel.restart()
+        }
     }
 
 }
